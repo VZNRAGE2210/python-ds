fbe302654c6fd883fde9dc80ad7cf9be440d707a
achintya kumar <achintyafzd@gmail.com>
2024-02-13 14:01:45 +0530

    controls

--- hello.py
+++ hello.py
@@ -1,1 +1,20 @@
-print('hello world')
+from turtle import*
+"""s = getscreen()
+t = Turtle()
+for i in range(8):
+    t.fd(100)
+    t.lt(45)
+    mainloop()"""
+"""fillcolor('red')
+begin_fill()
+for i in range(5):
+    fd(100)
+    lt(72)
+end_fill()
+fillcolor('yellow')
+begin_fill()
+circle(50)
+end_fill()
+mainloop()    """
+
+
