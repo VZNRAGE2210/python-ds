ed4de3a7547b5f8a7c818df36ef8fb6aa3cd13cf
achintya kumar <achintyafzd@gmail.com>
2024-03-28 19:57:15 +0530

    collage project

---
+++ collage_project/pages/about.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("SQLpad")
+st.markdown('hello people how are you guys doing  ')
+st.sidebar.image('https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/mvcufghlazueuq9peryr', use_column_width=True)
--- collage_project/pages/sqlpod.py
+++ collage_project/pages/sqlpod.py
@@ -1,7 +1,8 @@
 import ollama
 import streamlit as st
 
-st.title("Ollama Python Chatbot")
+st.title("SQLpod")
+st.sidebar.image('https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/mvcufghlazueuq9peryr', use_column_width=True)
 
 # initialize history
 if "messages" not in st.session_state:
@@ -23,8 +24,8 @@ def model_res_generator():
     for chunk in stream:
         yield chunk["message"]["content"]
 
-# Display chat messages from history on app rerun
-for message in st.session_state["messages"]:
+# Display chat messages from history on app re-run
+for message in st.session_state.get('messages', []):
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
@@ -38,3 +39,14 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         message = st.write_stream(model_res_generator())
         st.session_state["messages"].append({"role": "assistant", "content": message})
+
+st.sidebar.title("SQLpod History")
+st.sidebar.markdown('''
+    - Basic queries
+    - Employee table info
+''')
+
+
+
+
+    
