b1851ad495b4de52907554b3f386c143196e7b3f
achintya kumar <achintyafzd@gmail.com>
2024-04-05 20:46:51 +0530

    database

--- database.py
+++ database.py
@@ -1,27 +1,38 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from datetime import datetime
-Base = declarative_base()
+from sqlalchemy.orm import  declarative_base 
+from sqlalchemy import Column,Integer,String,ForeignKey,DateTime
+from datetime import datetime  
+Base =declarative_base()
 
-class user(Base):
-    __tablename__='user'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    email = Column(String(50), unique=True)
-    password = Column(String(64))
-    created_at = Column(DateTime,default = datetime.now)
+class User(Base):
+    __tablename__='users'
+    id=Column(Integer,primary_key=True)
+    name=Column(String(50))
+    email=Column(String(50),unique=True)
+    password=Column(String(64))
+    create_at=Column(DateTime,default=datetime.now)
+    
+class Message(Base):
+   __tablename__='messages'
+   id=Column(Integer,primary_key=True)
+   user_id=Column(Integer,ForeignKey('users.id'))
+   Message=Column(String(255))
+   created_at=Column(DateTime,default=datetime.now)
+
+def get_db():
+       engine =create_engine('sqlite:///example.db')
+       return sessionmaker(bind=engine)()
+   
+def save_to_db(object):
+       db=get_db()
+       db.add(object)
+       db.commit()
+       db.close()
 
 
-class message(Base):
-    __tablename__='message'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    message = Column(String(255))
-    created_at = Column(DateTime, default = datetime.now)
+       
 
 if __name__ == "__main__":
-    engine = create_engine('sqlite:///example.db')
+    engine =create_engine('sqlite:///example.db')
     Base.metadata.create_all(engine)
-
